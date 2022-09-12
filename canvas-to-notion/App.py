51448b1711ee2b9bcccbf0f9502ec5a279d6093a
Joseph Romanowski <joseph.romanowski@mymail.champlain.edu>
2022-09-11 23:01:58 -0400

    Allows info file to be selected and run

---
+++ canvas-to-notion/App.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import filedialog as fd
+import tkinterHelper.Console as Console
+from threading import Thread
+
+import CanvasToNotion as c2n
+
+class App:
+    def __init__(self) -> None:
+        self.window = Tk()
+        self.window.title("Canvas To Notion")
+
+        self.btn_fileSelect = Button(self.window, text="Browse", command=self.selectFile)
+        self.btn_fileSelect.place(x=10, y=20)
+
+        self.ent_fileNameEntry = Entry(self.window)
+        self.ent_fileNameEntry.place(x=70, y=25, width=260)
+
+        self.btn_run = Button(self.window, text="Run Program", command=self.run_thread)
+        self.btn_run.place(x=500, y=20)
+
+        self.cmd_console = Console.Console(self.window)
+        self.cmd_console.pack(pady=50)
+
+        print("Click browse and find your info.json")
+        self.window.mainloop()
+
+    def selectFile(self):
+        filetypes = (('Info File', '*.json'), ('All files', '*.*'))
+
+        f = fd.askopenfilename(title='Open Info.json', initialdir='.', filetypes=filetypes)
+        if f == '':
+            return
+
+        self.ent_fileNameEntry.delete(0)
+        self.ent_fileNameEntry.insert(0, f)
+
+    def run_thread(self):
+        filename = self.ent_fileNameEntry.get()
+        file = {}
+
+        try:
+            file = open(filename, mode='r')
+        except (FileNotFoundError):
+            print('File Not Found!')
+            return
+
+        self.thread = Thread(target=c2n.Run, args=[file])
+        self.thread.start()
---
+++ canvas-to-notion/CanvasToNotion.py
@@ -0,0 +1,50 @@
+import json
+from Notion.Property import Property
+from Notion.NotionWriter import NotionWriter
+from Canvas.CanvasReader import CanvasReader
+
+import time
+
+def Run(file):
+    info = json.load(file)
+
+    reader = CanvasReader(info['canvas_key'])
+    writer = NotionWriter(info['notion_token'], info['notion_database_id'])
+
+    print("Reading Notion database...")
+    writer.cache_pages()
+
+    # Gets each of the course ids from the info file 
+    for course_id_name in info['course_ids']:
+        if course_id_name['ignore']:
+            print(f"Ignoring assignments for course {course_id_name['name']}...")
+            continue
+
+        print(f"Reading assignments for course {course_id_name['name']}...")
+        assignments = reader.read_course_assignments(course_id_name['id'])
+
+        # Processes each of the assignments for the current course 
+        for a in assignments:
+            # Check ignore list 
+            ignored = False
+            for i in info['assignment_ignore_list']:
+                if (a['id'] == i['id']):
+                    print(f'    Ignoring assignment "{a["name"]}"...')
+                    ignored = True
+                    break
+            if ignored: continue
+
+            print(f'    Loading assignment "{a["name"]}"...')
+
+            properties = []
+
+            for format in info['property_formats']:
+                if format['assignment_value'] in a:
+                    if a[format['assignment_value']]: # this could be None 
+                        properties.append(Property(format['notion_name'], format['notion_type'], a[format['assignment_value']]))
+                else:
+                    raise Exception(f"Assignment value {format['assignment_value']} is not valid!")
+
+            writer.update_or_append(a['name'], properties)
+
+    print("\nSuccess!\n")
